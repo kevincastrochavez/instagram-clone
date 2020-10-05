c8964554241f5b7f4746173b45569456f7578fda
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-10-05 13:05:22 -0500

    created header with instagram logo, installed node-sass and styled some the document

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Instagram Clone</h1>
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__header-image"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="Instagram Logo"
+        />
+      </div>
     </div>
   );
 }
